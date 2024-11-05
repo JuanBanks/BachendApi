b2b83061557063104e38b467fea80580d79b1126
juancamilo2341431 <junadelacon23@gmail.com>
2024-11-04 19:35:52 -0500

    Update Server

--- routes/UsuariosRoutes.js
+++ routes/UsuariosRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Usuario = require('../models/Usuario');
+const Usuario = require('../models/Usuarios');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
